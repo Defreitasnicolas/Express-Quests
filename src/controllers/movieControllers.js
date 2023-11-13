e2e6c94bc62c433a3060886733af647783eab10e
Defreitasnicolas <defreitasnicolas@gmail.com>
2023-11-13 11:10:03 +0100

    methode put modifier des donnees

--- src/controllers/movieControllers.js
+++ src/controllers/movieControllers.js
@@ -27,6 +27,52 @@ const movies = [
   },
 ];
 
+const updateUsers = (req, res) => {
+  const id = parseInt(req.params.id);
+ const { firstname, lastname, email, city, language } = req.body;
+
+  database
+    .query(
+      "update users set firstname = ?, lastname = ?, email = ?, city = ?, language = ? where id = ?",
+      [firstname, lastname, email, city, language, id]
+    )
+    .then(([result]) => {
+      if (result.affectedRows === 0) {
+        res.sendStatus(404);
+      } else {
+        res.sendStatus(204);
+      }
+    })
+
+    .catch((err) => {
+      console.error(err);
+      res.sendStatus(500);
+    });
+};
+
+
+const updateMovie = (req, res) => {
+  const id = parseInt(req.params.id);
+  const { title, director, year, color, duration } = req.body;
+  
+  database
+    .query(
+      "update movies set title = ?, director = ?, year = ?, color = ?, duration = ? where id = ?",
+      [title, director, year, color, duration, id]
+    )
+    .then(([result]) => {
+      if (result.affectedRows === 0) {
+        res.sendStatus(404);
+      } else {
+        res.sendStatus(204);
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+      res.sendStatus(500);
+    });
+};
+
 const getMovies = (req, res) => {
   let sql = "select * from movies";
   const sqlValues = [];
@@ -153,6 +199,8 @@ const postUsers = (req, res) => {
     });
 };
 
+
+
 module.exports = {
   getMovies,
   getMovieById,
@@ -160,4 +208,6 @@ module.exports = {
   getUsersById,
   postMovie,
   postUsers,
+  updateMovie,
+  updateUsers,
 };
